337b3bbfd0130cad9665e7bc546971fed1bc3042
Rahul Trikha <rahul.trikha@gmail.com>
2011-02-09 01:38:11 +1030

    Initial Commit

---
+++ Classes/cameraAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  cameraAppDelegate.h
+//  camera
+//
+//  Created by Rahul Trikha on 8/02/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface cameraAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
